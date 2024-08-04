79c805e098f53942bb8d37222205a86bf0019a1a
Dxvarsh <borntobetwenty@gmail.com>
2024-08-04 11:52:44 +0530

    footer

--- src/script.js
+++ src/script.js
@@ -6,9 +6,7 @@ const loginbtn2 = document.getElementById("log-in-btn2")
 
 window.addEventListener("scroll",function(){
     const footer = document.querySelector('footer')
-    const footText = document.querySelector('#foot-text')
     footer.classList.toggle("h-20",window.scrollY > 0);
-    footText.classList.toggle("opacity-1",window.scrollY > 0);
 })
 
 // Function to fetch user information
